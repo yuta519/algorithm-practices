694c3dcf239f5eb16687004b2440303958084e5a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-23 00:02:37 +0900

    add calculated array for next step

--- sort_algorithm/count_sort.py
+++ sort_algorithm/count_sort.py
@@ -1,11 +1,12 @@
 def couting_sort(original_array: list[int]) -> list[int]:
     max_num: int = max(original_array)
     count_array: list[int] = [original_array.count(i) for i in range(0, max_num + 1)]
-    return count_array
-    # index_num: int = max_num
-    # while index_num >= 0:
-    #     if count_array[index_num] != 0:
-    #         index_num: int = index_num - 1
+    calculated_array: list[int] = []
+    base_int: int = 0
+    for i in count_array:
+        base_int += i
+        calculated_array.append(base_int)
+    return calculated_array
 
 
 if __name__ == "__main__":
