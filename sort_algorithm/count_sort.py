5ba8157ce162989493f73757923fcce9a568c88b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-23 14:34:20 +0900

    finish creating cout sort once

--- sort_algorithm/count_sort.py
+++ sort_algorithm/count_sort.py
@@ -1,12 +1,18 @@
 def couting_sort(original_array: list[int]) -> list[int]:
     max_num: int = max(original_array)
     count_array: list[int] = [original_array.count(i) for i in range(0, max_num + 1)]
+
     calculated_array: list[int] = []
     base_int: int = 0
     for i in count_array:
         base_int += i
         calculated_array.append(base_int)
-    return calculated_array
+
+    sorted_array: list[int] = [i for i in range(0, len(original_array))]
+    for i in reversed(original_array):
+        sorted_array[calculated_array[i] - 1] = i
+        calculated_array[i] -= 1
+    return sorted_array
 
 
 if __name__ == "__main__":
