b7abff9c3f463fbd62ab6e6377214d34803fe349
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-12-25 00:01:08 +0900

    improve chore

--- leetcode/lengthOfLongestSubstring.py
+++ leetcode/lengthOfLongestSubstring.py
@@ -4,19 +4,25 @@ def lengthOfLongestSubstring(s: str) -> int:
     :type s: str
     :rtype: int
     """
-    temp_array: list[str] = []
-    current_length: int = 0
-    max_length: int = 0
-    for i in s:
-        if i not in temp_array:
-            temp_array.append(i)
+    rtype: int = 0
+    for i in range(0, len(s) - 1):
+        temp_array: list[str] = []
+        current_length: int = 0
+        j: int = i
+        while len(temp_array) == len(set(temp_array)):
+            if j > len(s) - 1:
+                break
+            if len(temp_array) == 0:
+                current_length -= 1
+            print("index is ", j, ": ", s[j])
+            temp_array.append(s[j])
+            j += 1
             current_length += 1
-        else:
-            if max_length < current_length:
-                max_length = current_length
-            temp_array = []
-            current_length = 0
-    return max_length
+        print(temp_array, set(temp_array), current_length)
+        if rtype < current_length:
+            rtype = current_length
+            print("rtype is ", rtype)
+    return rtype
 
 
 if __name__ == "__main__":
@@ -25,3 +31,6 @@ if __name__ == "__main__":
 
     case2: str = "pwwkew"
     print(lengthOfLongestSubstring(case2))
+
+    case3: str = "bbbbb"
+    print(lengthOfLongestSubstring(case3))
